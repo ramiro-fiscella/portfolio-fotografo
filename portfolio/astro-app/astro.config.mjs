fedb39b9e19a4919c8260f6b2cde8c5b661b4ed4
ramiro-fiscella <ramirofiscella@gmail.com>
2025-04-15 18:41:35 -0300

    fixing deployment

--- portfolio/astro-app/astro.config.mjs
+++ portfolio/astro-app/astro.config.mjs
@@ -1,29 +1,30 @@
 // Loading environment variables from .env files
 // https://docs.astro.build/en/guides/configuring-astro/#environment-variables
-import { loadEnv } from "vite";
+import { loadEnv } from 'vite';
+
+import vercel from '@astrojs/vercel/static';
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
 const {
   PUBLIC_SANITY_STUDIO_PROJECT_ID,
   PUBLIC_SANITY_STUDIO_DATASET,
   PUBLIC_SANITY_PROJECT_ID,
   PUBLIC_SANITY_DATASET,
-} = loadEnv(import.meta.env.MODE, process.cwd(), "");
-import { defineConfig } from "astro/config";
+} = loadEnv(import.meta.env.MODE, process.cwd(), '');
+import { defineConfig } from 'astro/config';
 
 // Different environments use different variables
 const projectId = PUBLIC_SANITY_STUDIO_PROJECT_ID || PUBLIC_SANITY_PROJECT_ID;
 const dataset = PUBLIC_SANITY_STUDIO_DATASET || PUBLIC_SANITY_DATASET;
 
-import sanity from "@sanity/astro";
-import react from "@astrojs/react";
-
 // Change this depending on your hosting provider (Vercel, Netlify etc)
 // https://docs.astro.build/en/guides/server-side-rendering/#adding-an-adapter
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   // Hybrid+adapter is required to support embedded Sanity Studio
-  output: "hybrid",
+  output: 'static',
   adapter: vercel(),
   integrations: [
     sanity({
@@ -32,7 +33,7 @@ export default defineConfig({
       // studioBasePath: "/admin",
       useCdn: false,
       // `false` if you want to ensure fresh data
-      apiVersion: "2024-12-08", // Set to date of setup to use the latest API version
+      apiVersion: '2024-12-08', // Set to date of setup to use the latest API version
     }),
     react(), // Required for Sanity Studio
   ],
