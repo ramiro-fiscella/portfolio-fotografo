dfe6873484b8970498a54033241f0cef1e2c6708
ramiro-fiscella <ramirofiscella@gmail.com>
2025-04-15 19:10:12 -0300

    fixing deployment

--- portfolio/astro-app/astro.config.mjs
+++ portfolio/astro-app/astro.config.mjs
@@ -1,6 +1,10 @@
 // Loading environment variables from .env files
 // https://docs.astro.build/en/guides/configuring-astro/#environment-variables
 import { loadEnv } from 'vite';
+
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
 const {
   PUBLIC_SANITY_STUDIO_PROJECT_ID,
   PUBLIC_SANITY_STUDIO_DATASET,
@@ -12,9 +16,6 @@ import { defineConfig } from 'astro/config';
 // Different environments use different variables
 const projectId = PUBLIC_SANITY_STUDIO_PROJECT_ID || PUBLIC_SANITY_PROJECT_ID;
 const dataset = PUBLIC_SANITY_STUDIO_DATASET || PUBLIC_SANITY_DATASET;
-
-import sanity from '@sanity/astro';
-import react from '@astrojs/react';
 
 // Change this depending on your hosting provider (Vercel, Netlify etc)
 // https://docs.astro.build/en/guides/server-side-rendering/#adding-an-adapter
